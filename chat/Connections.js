6745ce1cd7344f3baab8cefb0861fe705965d68e
G.Ilshat <gaifullin.ilshat@gmail.com>
2018-10-26 01:28:15 +0500

    ws

--- chat/Connections.js
+++ chat/Connections.js
@@ -4,7 +4,7 @@ class Connections {
 
     constructor(options) {
         this._connections = [];
-        this._options = options || {port: 1337};
+        this._options = options || {port: 8000};
         this._configureSocket();
     }
 
